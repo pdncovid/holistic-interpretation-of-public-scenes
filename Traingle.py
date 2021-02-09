b1c751f107abcc51d8d07e08b34bc17380a16a4d
gihanjayatilaka <gihanjayatilaka@eng.pdn.ac.lk>
2021-02-09 11:59:42 +0530

    blueprint

--- Traingle.py
+++
@@ -1,9 +0,0 @@
-from Polygon import *
-class Triangle(Polygon): 
-  
-    # overriding abstract method 
-    def noofsides(self): 
-        print("I have 3 sides") 
-
-R = Triangle() 
-R.noofsides() 
