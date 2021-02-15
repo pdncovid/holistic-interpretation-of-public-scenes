67aad3b2d67958f1ac43c031eb08ffc2c05d351e
gihanjayatilaka <gihanjayatilaka@eng.pdn.ac.lk>
2021-02-16 00:30:59 +0530

    update

--- Visualize.py
+++ Visualize.py
@@ -2,6 +2,7 @@ import argparse
 from Graph import *
 from Person import *
 import matplotlib.pyplot as plt
+import numpy as np
 if __name__=="__main__":
 	args=argparse.ArgumentParser()
 	args.add_argument("--input","-i",type=str,dest="input")
@@ -13,19 +14,37 @@ if __name__=="__main__":
 	p = graph.getNode(args.person)
 	p = p.params
 
-	plt.scatter(p["X"],p["Y"])
+
+	fig, axs = plt.subplots(2)
+	fig.suptitle('Information of person : {}'.format(args.person))
+	# axs[0].plot(x, y)
+	# axs[1].plot(x, -y)
+
+	axs[0].scatter(p["X"][0],p["Y"][0],color='r')
+	axs[0].scatter(p["X"][1:-1],p["Y"][1:-1],color='g')
+	axs[0].scatter(p["X"][-1],p["Y"][-1],color='b')
+	axs[0].legend(["Start","Path","End"])
 
 	for a in range(min(len(p["X"]),len(p["Y"]))-1):
-		plt.arrow(p["X"][a],p["Y"][a],\
+		axs[0].arrow(p["X"][a],p["Y"][a],\
 			p["X"][a+1]-p["X"][a],p["Y"][a+1]-p["Y"][a])
 
-	plt.show()
+	# plt.show()
 
-	plt.figure()
+	# plt.figure()
 
-	k=p.keys()
-	k=k-"X"
-	k=k-"Y"
-	print(k)
+	keys=list(p.keys())
+	keys.remove("X")
+	keys.remove("Y")
+	print(keys)
 
+	toPlot=[]
+	for k in keys:
+		ar=np.array(p[k],dtype=np.float)
+		ar=ar-np.min(ar)
+		ar=ar/np.max(ar)
+		toPlot.append(ar)
+		axs[1].plot(np.arange(len(ar)),ar)
+
+	axs[1].legend(keys)
 	plt.show()	
