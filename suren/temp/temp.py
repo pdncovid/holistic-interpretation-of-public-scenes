9b250d5924a100cbec6a29ef2003c07cd9de802e
suren3141 <suren3141@gmail.com>
2021-03-20 22:45:24 +0530

    Graph represented as scatter

--- suren/temp/temp.py
+++ suren/temp/temp.py
@@ -1,13 +1,25 @@
 from suren.util import  Json
 import json
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+import time
 
 # with open('./nn-outputs/yoloOut.txt') as file:
 #     data = file.read()
 #     data = json.dump
 
-js = Json('../nn-outputs/yoloExp.txt')
 
-js_ = Json('../nn-outputs/sample-YOLO-bbox.json')
+plt.ion()
+x = np.arange(0, 4*np.pi, 0.1)
+y = [np.sin(i) for i in x]
+plt.plot(x, y, 'g-', linewidth=1.5, markersize=4)
+plt.draw()
+time.sleep(1)
+plt.plot(x, [i**2 for i in y], 'g-', linewidth=1.5, markersize=4)
+plt.pause(1)
+plt.plot(x, [i**2*i+0.25 for i in y], 'r-', linewidth=1.5, markersize=4) 
+plt.pause(1)
 
-js_.write(js.read())
-# print(js.read())
+plt.show(block=True)
+
