01bfc0493c7c9486b0a4a35668a797d6a8616a44
gihanjayatilaka <gihan@umd.edu>
2021-12-11 23:18:30 -0500

    eval updated to a working library

--- eval/eval.sh
+++ eval/eval.sh
@@ -1,13 +1,22 @@
 rm -rf ./temp/*
 mkdir ./temp
-mkdir ./temp/detections
-mkdir ./temp/groundtruths
-mkdir ./temp/plots
+mkdir ./temp/detection-results
+mkdir ./temp/ground-truth
 
 
 python eval.py --testType maskAgnostic
-cd ../submodules/Object-Detection-Metrics.git/
-python pascalvoc.py -gtformat xyrb -t 0.00 -gt ../../eval/temp/groundtruths/ -det ../../eval/temp/detections/ -sp ../../eval/temp/plots/
+
+
+cd ../submodules/mAP/
+rm -rf input/*
+cp -r ../../eval/temp/detection-results/ ./input/
+cp -r ../../eval/temp/ground-truth/ ./input/
+
+python main.py -np -na
+
+
+# cd ../submodules/Object-Detection-Metrics.git/
+# python pascalvoc.py -gtformat xyrb -t 0.00 -gt ../../eval/temp/groundtruths/ -det ../../eval/temp/detections/ -sp ../../eval/temp/plots/
 
 
 echo "PROGRAM END"
